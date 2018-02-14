3d546153666b9fefe3c361c03276243137459edd
grantg <gallagher.652@osu.edu>
2018-02-14 18:19:16 -0500

    initial commit

---
+++ app/src/main/java/ece/bikeblindspotdetector/MainActivity.java
@@ -0,0 +1,13 @@
+package ece.bikeblindspotdetector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
