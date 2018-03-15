7c987e7ab9af93302dbdd86a943811830337ca26
grantmgallagher <gallagher.652@osu.edu>
2018-03-15 12:50:06 -0400

    Added Fragments for the VDP stats and the Traffic Viewer

--- app/src/main/java/ece/bikeblindspotdetector/NavDrawer.java
+++ app/src/main/java/ece/bikeblindspotdetector/NavDrawer.java
@@ -3,6 +3,8 @@ package ece.bikeblindspotdetector;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class NavDrawer extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -79,15 +82,30 @@ public class NavDrawer extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        Fragment fragment = null;
+        if (id == R.id.traffic_viewer) {
+            fragment = new TrafficViewerFragment();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
+        } else if (id == R.id.vdp_stats){
+            fragment = new VDPStatsFragment();
         }
-
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame, fragment);
+            ft.commit();
+        }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+    public void changeRed(Button button)
+    {
+
+    }
+    public void changeGreen(Button button)
+    {
+
     }
 }
